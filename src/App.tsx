80af8ccd9f70dffed41ee24fd983e8bd218facdb
Talita Moulin <talitamoulin11@gmail.com>
2026-03-13 00:05:09 -0300

    Retirada das rotas, correção de erros e modificações nos textos.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Header } from "./components/Header";
 import Hero from "./components/Hero";
 import { Contato } from "./components/Contact";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="min-h-screen bg-orange-50 font-sans text-gray-800 selection:bg-orange-200">
-        <Header />
-        <Hero />
-        <Contato />
-      </div>
-    </BrowserRouter>
+    <div className="min-h-screen bg-orange-50 font-sans text-gray-800 selection:bg-orange-200">
+      <Header />
+      <Hero />
+      <Contato />
+    </div>
   );
 }
 
